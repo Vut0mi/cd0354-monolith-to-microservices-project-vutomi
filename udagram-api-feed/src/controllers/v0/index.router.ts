cdd9d36074bd11172cc5930821a63dc8b0970fb2
Vut0mi <lifenxumalo@gmail.com>
2025-05-17 12:52:03 +0200

    update

--- udagram-api-feed/src/controllers/v0/index.router.ts
+++ udagram-api-feed/src/controllers/v0/index.router.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express';
-import { UserRouter } from './users/routes/user.router';
+import { FeedRouter } from './feed/routes/feed.router';
 
 const router: Router = Router();
 
-// Mount UserRouter under /users
-router.use('/users', UserRouter);
+// Mount FeedRouter under /feed
+router.use('/feed', FeedRouter);
 
-// Simple base route
+// Base route
 router.get('/', async (req: Request, res: Response) => {
   res.send('V0');
 });
